eac63656fd21b74ead1f5f9285807c4960b2095d
Wenzel, Toni <toni.wenzel@chg-meridian.com>
2018-10-26 13:03:20 +0200

    Provide sample output

--- samples/Registry/Controllers/HomeController.cs
+++ samples/Registry/Controllers/HomeController.cs
@@ -5,33 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Registry.Models;
+using ServiceGovernance.Registry.Stores;
 
 namespace Registry.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IServiceStore _serviceStore;
+
+        public HomeController(IServiceStore serviceStore)
         {
-            return View();
+            _serviceStore = serviceStore ?? throw new ArgumentNullException(nameof(serviceStore));
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> Index()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return View(await _serviceStore.GetAllAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
